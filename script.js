a004ed78ee558b960934e50375f2ed3c9596cf7d
Luan <113206529+luanleft@users.noreply.github.com>
2025-05-12 18:32:55 -0300

    commit archives

---
+++ script.js
@@ -0,0 +1,6 @@
+let botao = document.querySelector('button');
+let mensagem = document.querySelector('.mensagem')
+
+botao.addEventListener('click', (e) =>{
+    mensagem.innerHTML = "Esse sou eu me especializando em programar, você também consegue!"
+});
